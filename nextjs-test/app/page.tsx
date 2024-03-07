d6df4c935143fedffbeece342659a92877801fc9
linchen1987 <linchen.1987@foxmail.com>
2024-03-07 10:35:50 +0800

    update nextjs-test

--- nextjs-test/app/page.tsx
+++ nextjs-test/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 
 export default function Home() {
@@ -38,6 +40,14 @@ export default function Home() {
           priority
         />
       </div>
+
+      <button
+        onClick={() => {
+          console.log("Hello next.js!");
+        }}
+      >
+        Click me and see log in browser console
+      </button>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
